1c2766178efc6bcd1f7cdebe7e9e48b8496f9509
CharlesMengCA <CharlesMeng@outlook.com>
2022-09-15 23:12:28 -0400

    Sort Support

--- WinMergeScript.cpp
+++ WinMergeScript.cpp
@@ -2,11 +2,12 @@
 #include "stdafx.h"
 #include "DisplayJSONFiles.h"
 #include "WinMergeScript.h"
-#include "rapidjson/include/rapidjson/reader.h"
 #include "rapidjson/include/rapidjson/prettywriter.h"
 #include "rapidjson/include/rapidjson/filereadstream.h"
 #include "rapidjson/include/rapidjson/filewritestream.h"
-#include "rapidjson/include/rapidjson/error/en.h"
+#include "rapidjson/include/rapidjson/document.h"
+
+#include <algorithm>
 
 using namespace rapidjson;
 
@@ -14,51 +15,129 @@ using namespace rapidjson;
 // CWinMergeScript
 
 
-STDMETHODIMP CWinMergeScript::get_PluginEvent(BSTR *pVal)
+STDMETHODIMP CWinMergeScript::get_PluginEvent(BSTR* pVal)
 {
 	*pVal = SysAllocString(L"FILE_PACK_UNPACK");
 	return S_OK;
 }
 
-STDMETHODIMP CWinMergeScript::get_PluginDescription(BSTR *pVal)
+STDMETHODIMP CWinMergeScript::get_PluginDescription(BSTR* pVal)
 {
 	*pVal = SysAllocString(L"Prettify JSON files so WinMerge can compare them");
 	return S_OK;
 }
 
-STDMETHODIMP CWinMergeScript::get_PluginFileFilters(BSTR *pVal)
+STDMETHODIMP CWinMergeScript::get_PluginFileFilters(BSTR* pVal)
 {
 	*pVal = SysAllocString(L"\\.json$");
 	return S_OK;
 }
 
-STDMETHODIMP CWinMergeScript::get_PluginIsAutomatic(VARIANT_BOOL *pVal)
+STDMETHODIMP CWinMergeScript::get_PluginIsAutomatic(VARIANT_BOOL* pVal)
 {
 	*pVal = VARIANT_TRUE;
 	return S_OK;
 }
 
 
-STDMETHODIMP CWinMergeScript::UnpackBufferA(SAFEARRAY **pBuffer, INT *pSize, VARIANT_BOOL *pbChanged, INT *pSubcode, VARIANT_BOOL *pbSuccess)
+STDMETHODIMP CWinMergeScript::UnpackBufferA(SAFEARRAY** pBuffer, INT* pSize, VARIANT_BOOL* pbChanged, INT* pSubcode, VARIANT_BOOL* pbSuccess)
 {
 	// We don't need it
 	return S_OK;
 }
 
-STDMETHODIMP CWinMergeScript::PackBufferA(SAFEARRAY **pBuffer, INT *pSize, VARIANT_BOOL *pbChanged, INT subcode, VARIANT_BOOL *pbSuccess)
+STDMETHODIMP CWinMergeScript::PackBufferA(SAFEARRAY** pBuffer, INT* pSize, VARIANT_BOOL* pbChanged, INT subcode, VARIANT_BOOL* pbSuccess)
 {
 	// We don't need it
 	return S_OK;
 }
 
-STDMETHODIMP CWinMergeScript::UnpackFile(BSTR fileSrc, BSTR fileDst, VARIANT_BOOL *pbChanged, INT *pSubcode, VARIANT_BOOL *pbSuccess)
+template<typename Criteria>
+void SortPropertiesOfObject(rapidjson::Value& v, const Criteria& criteria)
+{
+	std::sort(v.MemberBegin(), v.MemberEnd(), criteria);
+}
+
+template<typename Criteria>
+void SortItemsOfArray(rapidjson::Value& v, const Criteria& criteria)
+{
+	std::sort(v.Begin(), v.End(), criteria);
+}
+
+auto criteriaObject = [](const Value::Member& left, const Value::Member& right) -> bool
+{
+	const char* leftStr = left.name.GetString();
+	const char* rightStr = right.name.GetString();
+
+	return strcmp(leftStr, rightStr) < 0;
+};
+
+auto criteriaArrayObject = [](const Value& left, const Value& right) -> bool
+{
+	const char* leftStr = left.MemberBegin()->name.GetString();
+	const char* rightStr = right.MemberBegin()->name.GetString();
+
+	return strcmp(leftStr, rightStr) < 0;
+};
+
+auto criteriaNumber = [](const Value& left, const Value& right)
+{
+	return left.GetDouble() < right.GetDouble();
+};
+
+auto criteriaString = [](const Value& left, const Value& right)
+{
+	return strcmp(left.GetString(), right.GetString()) < 0;
+};
+
+auto criteriaType = [](const Value& left, const Value& right)
+{
+	Type leftType = left.GetType();
+	Type rightType = right.GetType();
+
+	return leftType < rightType;
+};
+
+void SortValue(rapidjson::Value& d) {
+
+	if (d.IsObject()) {
+
+		for (Value::MemberIterator itr = d.MemberBegin(); itr != d.MemberEnd(); ++itr)
+		{
+			if (itr->value.IsArray()) SortValue(itr->value);
+		}
+
+		SortPropertiesOfObject(d, criteriaObject);
+	}
+	else if (d.IsArray()) {
+
+		if (d[0].IsNumber()) {
+			SortItemsOfArray(d, criteriaNumber);
+		}
+		else if (d[0].IsString()) {
+			SortItemsOfArray(d, criteriaString);
+		}
+		else if (d[0].IsObject()) {
+
+			for (SizeType i = 0; i < d.Size(); i++)
+			{
+				SortValue(d[i]);
+			}
+
+			SortItemsOfArray(d, criteriaArrayObject);
+		}
+
+		SortItemsOfArray(d, criteriaType);
+	}
+}
+
+STDMETHODIMP CWinMergeScript::UnpackFile(BSTR fileSrc, BSTR fileDst, VARIANT_BOOL* pbChanged, INT* pSubcode, VARIANT_BOOL* pbSuccess)
 {
 	// Prepare reader and input stream.
 	FILE* fi, * fo;
 	char* readBuffer, * writeBuffer;
 
 	_wfopen_s(&fi, fileSrc, L"rb");
-	Reader reader;
 	readBuffer = (char*)malloc(65536);
 	FileReadStream is(fi, readBuffer, sizeof(readBuffer));
 
@@ -68,15 +147,21 @@ STDMETHODIMP CWinMergeScript::UnpackFile(BSTR fileSrc, BSTR fileDst, VARIANT_BOO
 	FileWriteStream os(fo, writeBuffer, sizeof(writeBuffer));
 	PrettyWriter<FileWriteStream> writer(os);
 
-	// JSON reader parse from the input stream and let writer generate the output.
-	if (reader.Parse<kParseValidateEncodingFlag>(is, writer)) {
-		*pbChanged = VARIANT_TRUE;
-		*pbSuccess = VARIANT_TRUE;
-	}
-	else
+	Document d;
+
+	if (d.ParseStream<kParseCommentsFlag>(is).HasParseError())
 	{
 		*pbChanged = VARIANT_FALSE;
 		*pbSuccess = VARIANT_FALSE;
+	}
+	else
+	{
+		SortValue(d);
+
+		d.Accept(writer);
+
+		*pbChanged = VARIANT_TRUE;
+		*pbSuccess = VARIANT_TRUE;
 	}
 
 	free(readBuffer);
@@ -90,14 +175,13 @@ STDMETHODIMP CWinMergeScript::UnpackFile(BSTR fileSrc, BSTR fileDst, VARIANT_BOO
 
 
 
-STDMETHODIMP CWinMergeScript::PackFile(BSTR fileSrc, BSTR fileDst, VARIANT_BOOL *pbChanged, INT pSubcode, VARIANT_BOOL *pbSuccess)
+STDMETHODIMP CWinMergeScript::PackFile(BSTR fileSrc, BSTR fileDst, VARIANT_BOOL* pbChanged, INT pSubcode, VARIANT_BOOL* pbSuccess)
 {
 	// Prepare reader and input stream.
 	FILE* fi, * fo;
 	char* readBuffer, * writeBuffer;
 
 	_wfopen_s(&fi, fileSrc, L"rb");
-	Reader reader;
 	readBuffer = (char*)malloc(65536);
 	FileReadStream is(fi, readBuffer, sizeof(readBuffer));
 
@@ -107,15 +191,19 @@ STDMETHODIMP CWinMergeScript::PackFile(BSTR fileSrc, BSTR fileDst, VARIANT_BOOL 
 	FileWriteStream os(fo, writeBuffer, sizeof(writeBuffer));
 	Writer<FileWriteStream> writer(os);
 
-	// JSON reader parse from the input stream and let writer generate the output.
-	if (reader.Parse<kParseValidateEncodingFlag>(is, writer)) {
-		*pbChanged = VARIANT_TRUE;
-		*pbSuccess = VARIANT_TRUE;
-	}
-	else
+	Document d;
+
+	if (d.ParseStream<kParseCommentsFlag>(is).HasParseError())
 	{
 		*pbChanged = VARIANT_FALSE;
 		*pbSuccess = VARIANT_FALSE;
+	}
+	else
+	{
+		d.Accept(writer);
+
+		*pbChanged = VARIANT_TRUE;
+		*pbSuccess = VARIANT_TRUE;
 	}
 
 	free(readBuffer);
@@ -125,10 +213,9 @@ STDMETHODIMP CWinMergeScript::PackFile(BSTR fileSrc, BSTR fileDst, VARIANT_BOOL 
 	if (fo) fclose(fo);
 
 	return S_OK;
-
 }
 
-STDMETHODIMP CWinMergeScript::ShowSettingsDialog(VARIANT_BOOL *pbHandled)
+STDMETHODIMP CWinMergeScript::ShowSettingsDialog(VARIANT_BOOL* pbHandled)
 {
 	*pbHandled = VARIANT_FALSE;
 	return E_NOTIMPL;
